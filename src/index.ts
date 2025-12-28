42378f498aef778648848b69297bbdb5ef0fc1b8
Nazita <laraianazareno@gmail.com>
2025-12-28 15:46:26 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", allowOrigin);
   res.header("Vary", "Origin");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   if (req.method === "OPTIONS") {
     res.sendStatus(204);
     return;
